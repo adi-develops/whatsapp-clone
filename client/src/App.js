6d63d009d9262057fbed00b9a48fe9a60820e71f
Aryaman <201b064@juetguna.in>
2023-02-03 00:05:46 +0530

    Info Drawer and Empty Chat Panel

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Messenger from "./components/Messenger";
-import AccountProvider from "./context/accountprovider";
+import AccountProvider from "./context/AccountProvider";
 
 function App() {
   const clientId = "396490456287-fsgjbr1nsel0couif34eluaduomagura.apps.googleusercontent.com";
--- client/src/components/chat/chat/EmptyChat.jsx
+++ client/src/components/chat/chat/EmptyChat.jsx
@@ -1,8 +1,54 @@
+import { emptyChatImage } from "../../../constants/data";
+import { Box, Typography, styled, Divider } from "@mui/material";
 
+
+const Component=styled(Box)`
+    background: #f8f9f8;
+    padding: 30px 0;
+    text-align: center;
+    height: 100vh;
+`;
+
+const Container =styled(Box)`
+    padding: 0 200px;
+
+` 
+const Image =styled('img')({
+    width:400,
+    marginTop:100
+});
+
+const Title=styled(Typography)`
+    font-size: 32px;
+    margin: 25px 0 10px 0;
+    font-family: inherit;
+    font-weight: 300;
+    color: #41525d;
+
+`;
+
+const SubTitle= styled(Typography)`
+    font-size:14px;
+    color:#667781;
+    font-weight:400;
+    font-family:inherit;
+`;
+
+const StyledDivider=styled(Divider)`
+    margin:40px 0;
+    opacity:0.4;
+`
 
 const EmptyChat = () => {
     return(
-        <div>Hello From EmptyChat</div>
+        <Component>
+            <Container>
+                <Image src={emptyChatImage} alt ="image"/>
+                <Title>NovoChat</Title>
+                <SubTitle>Send and receive messages and media using your Google account only.</SubTitle>
+                <StyledDivider/>
+            </Container>
+        </Component>
     )
 }
 
